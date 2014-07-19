ceccf8ef6809fd29275b6d11206873e664156160
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-07-19 14:52:49 +0200

    Implemented

--- app/src/main/java/com/alorma/github/ui/fragment/repos/ReposFragment.java
+++ app/src/main/java/com/alorma/github/ui/fragment/repos/ReposFragment.java
@@ -1,17 +1,19 @@
 package com.alorma.github.ui.fragment.repos;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.alorma.github.sdk.bean.dto.response.ListRepos;
 import com.alorma.github.sdk.bean.dto.response.Repo;
 import com.alorma.github.sdk.services.repos.BaseReposClient;
 import com.alorma.github.sdk.services.repos.UserReposClient;
 import com.alorma.github.ui.adapter.repos.ReposAdapter;
+import com.alorma.github.ui.fragment.base.PaginatedFabListFragment;
 import com.alorma.github.ui.fragment.base.PaginatedListFragment;
 
 import java.util.ArrayList;
 
-public class ReposFragment extends PaginatedListFragment<ListRepos> {
+public class ReposFragment extends PaginatedFabListFragment<ListRepos> {
     private static final String COLOR = "COLOR";
 
     private String username;
@@ -32,6 +34,7 @@ public class ReposFragment extends PaginatedListFragment<ListRepos> {
         }
         return reposFragment;
     }
+
     public static ReposFragment newInstance(String username, int color) {
         ReposFragment reposFragment = new ReposFragment();
         if (username != null) {
@@ -45,13 +48,18 @@ public class ReposFragment extends PaginatedListFragment<ListRepos> {
     }
 
     @Override
-    protected void executeRequest() {
-        BaseReposClient client;
-
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             username = getArguments().getString(USERNAME);
             textColor = getArguments().getInt(COLOR);
         }
+        setUseFab(username == null);
+    }
+
+    @Override
+    protected void executeRequest() {
+        BaseReposClient client;
 
         client = new UserReposClient(getActivity(), username);
 
@@ -91,4 +99,8 @@ public class ReposFragment extends PaginatedListFragment<ListRepos> {
         }
     }
 
+    @Override
+    protected void onFabClick() {
+        Toast.makeText(getActivity(), "Hello FAB", Toast.LENGTH_SHORT).show();
+    }
 }
