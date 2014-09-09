d3371c9c36a461a2d217ed851f09c65e1ce5ce03
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-09-09 12:30:57 +0200

    Fix bug crash on comment issue
    There was a problem with parcelable and inerithance.

--- sdk/src/main/java/com/alorma/github/sdk/bean/info/IssueInfo.java
+++ sdk/src/main/java/com/alorma/github/sdk/bean/info/IssueInfo.java
@@ -6,24 +6,44 @@ import android.os.Parcelable;
 /**
  * Created by Bernat on 06/09/2014.
  */
-public class IssueInfo extends RepoInfo implements Parcelable{
-	public int num;
+public class IssueInfo implements Parcelable{
 
+	public String owner;
 	public String repo;
+	public int num;
 
 	public IssueInfo() {
 	}
 
 	public IssueInfo(Parcel in) {
-		super(in);
-
+		owner = in.readString();
+		repo = in.readString();
 		num = in.readInt();
 	}
 
 	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
 	public void writeToParcel(Parcel dest, int flags) {
-		super.writeToParcel(dest, flags);
+		dest.writeString(owner);
+		dest.writeString(repo);
 		dest.writeInt(num);
 	}
 
+
+
+	public static final Parcelable.Creator<IssueInfo> CREATOR = new Parcelable.Creator<IssueInfo>() {
+		@Override
+		public IssueInfo createFromParcel(Parcel in) {
+			return new IssueInfo(in);
+		}
+
+		@Override
+		public IssueInfo[] newArray(int size) {
+			return new IssueInfo[size];
+		}
+	};
 }
