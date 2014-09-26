1ee12e128f996f39d166fd3c9a6e6f0be7245586
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-09-26 19:30:27 +0200

    Update repositories fragments and add files to sort repositores

--- app/src/main/java/com/alorma/github/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/alorma/github/ui/activity/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.alorma.github.ui.activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 
+import com.alorma.github.R;
 import com.alorma.github.ui.activity.base.BackActivity;
 import com.alorma.github.ui.fragment.preference.GitskariosPreferenceFragment;
 
@@ -12,12 +13,14 @@ public class SettingsActivity extends BackActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings_activity);
 
 		FragmentTransaction ft = getFragmentManager().beginTransaction();
 		ft.replace(android.R.id.content, new GitskariosPreferenceFragment());
 		ft.commit();
 
 	}
+
 
 
 }
