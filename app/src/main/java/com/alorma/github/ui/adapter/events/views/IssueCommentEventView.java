568d97961825c3ec193556deeb389f0dd2a48e64
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-10-05 13:55:31 +0200

    Issue comment view

---
+++ app/src/main/java/com/alorma/github/ui/adapter/events/views/IssueCommentEventView.java
@@ -0,0 +1,69 @@
+package com.alorma.github.ui.adapter.events.views;
+
+import android.content.Context;
+import android.text.Html;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.alorma.github.R;
+import com.alorma.github.sdk.bean.dto.response.Commit;
+import com.alorma.github.sdk.bean.dto.response.GithubEvent;
+import com.alorma.github.sdk.bean.dto.response.events.payload.IssueCommentEventPayload;
+import com.google.gson.Gson;
+import com.joanzapata.android.iconify.IconDrawable;
+import com.joanzapata.android.iconify.Iconify;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+/**
+ * Created by Bernat on 05/10/2014.
+ */
+public class IssueCommentEventView extends GithubEventView<IssueCommentEventPayload> {
+
+	public IssueCommentEventView(Context context) {
+		super(context);
+	}
+
+	public IssueCommentEventView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public IssueCommentEventView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	protected void inflate() {
+		inflate(getContext(), R.layout.payload_issue_comment, this);
+	}
+
+	@Override
+	protected void populateView(GithubEvent event) {
+		TextView actionType = (TextView) findViewById(R.id.actionType);
+		actionType.setText(R.string.issue_commented);
+
+		ImageView authorAvatar = (ImageView) findViewById(R.id.authorAvatar);
+
+		ImageLoader.getInstance().displayImage(event.actor.avatar_url, authorAvatar);
+
+		TextView authorName = (TextView) findViewById(R.id.authorName);
+		authorName.setText(event.actor.login);
+
+		TextView comment = (TextView) findViewById(R.id.comment);
+
+		comment.setText(Html.fromHtml(eventPayload.comment.body));
+
+		IconDrawable left = new IconDrawable(getContext(), Iconify.IconValue.fa_code);
+
+		TextView action = (TextView) findViewById(R.id.action);
+		action.setCompoundDrawables(left, null, null, null);
+
+		action.setText(eventPayload.issue.title);
+	}
+
+	@Override
+	protected IssueCommentEventPayload convert(Gson gson, String s) {
+		return gson.fromJson(s, IssueCommentEventPayload.class);
+	}
+}
