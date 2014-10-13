863630a7b4d6b1a6b282a480b44bd725afb069d8
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-10-13 19:15:01 +0200

    New repo activity

--- app/src/main/java/com/alorma/github/ui/activity/NewRepoActivity.java
+++ app/src/main/java/com/alorma/github/ui/activity/NewRepoActivity.java
@@ -4,7 +4,7 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 import com.alorma.github.ui.activity.base.BackActivity;
-import com.alorma.github.ui.fragment.detail.repo.RepoSettingsFragment;
+import com.alorma.github.ui.fragment.detail.repo.RepoCreateFragment;
 
 /**
  * Created by Bernat on 07/10/2014.
@@ -16,6 +16,7 @@ public class NewRepoActivity extends BackActivity{
 		super.onCreate(savedInstanceState);
 
 		FragmentTransaction ft = getFragmentManager().beginTransaction();
-		ft.replace(android.R.id.content, RepoSettingsFragment.newInstance(null));
+		ft.replace(android.R.id.content, RepoCreateFragment.newInstance());
+		ft.commit();
 	}
 }
