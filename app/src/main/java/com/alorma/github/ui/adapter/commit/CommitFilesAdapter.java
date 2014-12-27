ee089d3ed57099fe98ab07c5c19019f9a91bfbc7
Bernat Borrás Paronella <bernatbor15@gmail.com>
2014-12-28 00:11:01 +0100

    Commits list & patch

--- app/src/main/java/com/alorma/github/ui/adapter/commit/CommitFilesAdapter.java
+++ app/src/main/java/com/alorma/github/ui/adapter/commit/CommitFilesAdapter.java
@@ -1,19 +1,24 @@
 package com.alorma.github.ui.adapter.commit;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alorma.diff.lib.DiffTextView;
 import com.alorma.github.R;
 import com.alorma.github.sdk.bean.dto.response.CommitFile;
 import com.alorma.github.sdk.bean.dto.response.GitCommitFiles;
+import com.alorma.github.ui.activity.FileActivity;
 import com.alorma.github.ui.utils.UniversalImageLoaderUtils;
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.io.File;
 
 /**
  * Created by Bernat on 22/12/2014.
@@ -40,11 +45,29 @@ public class CommitFilesAdapter extends RecyclerView.Adapter<CommitFilesAdapter.
 	@Override
 	public void onBindViewHolder(FileVH holder, int position) {
 		CommitFile commitFile = files.get(position);
-		holder.fileName.setText(commitFile.filename);
+		String fileName = getFileName(commitFile.filename);
+		if (fileName != null) {
+			holder.fileName.setText(fileName);
+		}
+		
 		if (commitFile.patch != null) {
 			holder.diffTextView.setMaxLines(8);
 			holder.diffTextView.setText(commitFile.patch);
 		}
+	}
+
+	private String getFileName(String filename) {
+		if (filename != null) {
+			String[] names = filename.split(File.separator);
+			if (names.length > 1) {
+				int last = names.length - 1;
+				return names[last];
+			} else {
+				return names[0];
+			}
+		}
+
+		return null;
 	}
 
 	@Override
@@ -61,12 +84,14 @@ public class CommitFilesAdapter extends RecyclerView.Adapter<CommitFilesAdapter.
 			super(itemView);
 			diffTextView = (DiffTextView) itemView.findViewById(R.id.diffText);
 			fileName = (TextView) itemView.findViewById(R.id.fileName);
+			itemView.setOnClickListener(this);
 			diffTextView.setOnClickListener(this);
 		}
 
 		@Override
 		public void onClick(View v) {
-
+			Intent launcherIntent = FileActivity.createLauncherIntent(v.getContext(), files.get(getPosition()).patch);
+			v.getContext().startActivity(launcherIntent);
 		}
 	}
 }
