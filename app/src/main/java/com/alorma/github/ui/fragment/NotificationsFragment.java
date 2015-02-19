92ba800215480603f9386257f7cc915bbe96caff
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-02-19 01:50:04 +0100

    Load issues (and repos) from notifications

--- app/src/main/java/com/alorma/github/ui/fragment/NotificationsFragment.java
+++ app/src/main/java/com/alorma/github/ui/fragment/NotificationsFragment.java
@@ -1,18 +1,31 @@
 package com.alorma.github.ui.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.alorma.github.R;
 import com.alorma.github.sdk.bean.dto.Notification;
+import com.alorma.github.sdk.bean.dto.response.GithubEvent;
+import com.alorma.github.sdk.bean.dto.response.Issue;
+import com.alorma.github.sdk.bean.dto.response.events.EventType;
+import com.alorma.github.sdk.bean.dto.response.events.payload.IssueCommentEventPayload;
+import com.alorma.github.sdk.bean.info.IssueInfo;
+import com.alorma.github.sdk.bean.info.RepoInfo;
 import com.alorma.github.sdk.services.notifications.GetNotificationsClient;
+import com.alorma.github.ui.activity.IssueDetailActivity;
+import com.alorma.github.ui.activity.RepoDetailActivity;
 import com.alorma.github.ui.adapter.NotificationsAdapter;
 import com.alorma.github.ui.fragment.base.PaginatedListFragment;
 import com.alorma.github.ui.view.DirectionalScrollListener;
 import com.alorma.githubicons.GithubIconify;
+import com.google.gson.Gson;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -27,6 +40,7 @@ import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 public class NotificationsFragment extends PaginatedListFragment<List<Notification>> {
 
 	private StickyListHeadersListView listView;
+	public NotificationsAdapter notificationsAdapter;
 
 	public static NotificationsFragment newInstance() {
 		return new NotificationsFragment();
@@ -47,6 +61,7 @@ public class NotificationsFragment extends PaginatedListFragment<List<Notificati
 			listView.setOnScrollListener(new DirectionalScrollListener(this, this, FAB_ANIM_DURATION));
 			listView.setOnItemClickListener(this);
 			listView.setAreHeadersSticky(false);
+			listView.setOnItemClickListener(this);
 		}
 	}
 
@@ -81,9 +96,10 @@ public class NotificationsFragment extends PaginatedListFragment<List<Notificati
 				Collections.sort(notifications, Notification.Comparators.REPO_ID);
 
 				getActivity().runOnUiThread(new Runnable() {
+
 					@Override
 					public void run() {
-						NotificationsAdapter notificationsAdapter = new NotificationsAdapter(getActivity(), notifications);
+						notificationsAdapter = new NotificationsAdapter(getActivity(), notifications);
 						listView.setAdapter(notificationsAdapter);
 					}
 				});
@@ -107,5 +123,32 @@ public class NotificationsFragment extends PaginatedListFragment<List<Notificati
 	@Override
 	protected int getNoDataText() {
 		return R.string.no_notifications;
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		Notification item = notificationsAdapter.getItem(position);
+		String type = item.subject.type;
+		
+		// https://api.github.com/repos/github/android/issues/666
+		Uri uri = Uri.parse(item.subject.url);
+		if (type.equalsIgnoreCase("Issue")) {
+			List<String> segments = uri.getPathSegments();
+			String user = segments.get(1);
+			String repo = segments.get(2);
+			String number = segments.get(4);
+			IssueInfo issueInfo = new IssueInfo();
+			issueInfo.num = Integer.valueOf(number);
+			issueInfo.repo = new RepoInfo();
+			issueInfo.repo.owner = user;
+			issueInfo.repo.name = repo;
+			Intent launcherIntent = IssueDetailActivity.createLauncherIntent(getActivity(), issueInfo, item.repository.permissions);
+			startActivity(launcherIntent);
+		} else {
+			String fullName = item.repository.full_name;
+			String[] parts = fullName.split("/");
+			Intent intent = RepoDetailActivity.createLauncherIntent(getActivity(), parts[0], parts[1]);
+			startActivity(intent);
+		}
 	}
 }
