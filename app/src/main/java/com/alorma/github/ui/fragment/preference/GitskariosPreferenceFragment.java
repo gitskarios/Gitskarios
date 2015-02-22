6ad954ba2466a65768905e1af91ac774b06efcfe
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-02-22 22:24:17 +0100

    Default values

--- app/src/main/java/com/alorma/github/ui/fragment/preference/GitskariosPreferenceFragment.java
+++ app/src/main/java/com/alorma/github/ui/fragment/preference/GitskariosPreferenceFragment.java
@@ -2,38 +2,28 @@ package com.alorma.github.ui.fragment.preference;
 
 import android.content.ComponentName;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 
 import com.alorma.github.Interceptor;
 import com.alorma.github.R;
-import com.alorma.github.inapp.IabConstants;
-import com.alorma.github.inapp.IabHelper;
-import com.alorma.github.inapp.IabResult;
-import com.alorma.github.inapp.Inventory;
-import com.alorma.github.inapp.Purchase;
 import com.alorma.github.sdk.security.StoreCredentials;
 import com.alorma.github.sdk.utils.GitskariosSettings;
 import com.alorma.github.ui.activity.LoginActivity;
+import com.alorma.github.ui.activity.RepoDetailActivity;
 
-public class GitskariosPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener
-		/*, IabHelper.OnIabSetupFinishedListener,
-		IabHelper.OnIabPurchaseFinishedListener, IabHelper.QueryInventoryFinishedListener*/ {
+public class GitskariosPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
 
 	private static final String PREF_INTERCEPT = "pref_intercept";
 	public static final String REPOS_SORT = "repos_sort";
 	public static final String REPOS_FILE_TYPE = "repos_download_type";
 	public static final String REAUTHORIZE = "reauthorize";
-//	public static final String DONATE = "donate";
+	public static final String GITSKARIOS = "gitskarios";
 
 	private StoreCredentials credentials;
-	/*private IabHelper iabHelper;
-	private boolean iabEnabled;*/
-	private Preference donate;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -41,7 +31,8 @@ public class GitskariosPreferenceFragment extends PreferenceFragment implements 
 
 		addPreferencesFromResource(R.xml.main_prefs);
 
-		findPreference(PREF_INTERCEPT).setOnPreferenceChangeListener(this);
+		CheckBoxPreference intercetor = (CheckBoxPreference) findPreference(PREF_INTERCEPT);
+		intercetor.setOnPreferenceChangeListener(this);
 
 		findPreference(REPOS_SORT).setOnPreferenceChangeListener(this);
 
@@ -57,17 +48,9 @@ public class GitskariosPreferenceFragment extends PreferenceFragment implements 
 			reauthorize.setOnPreferenceClickListener(this);
 		}
 
-		//donate = findPreference(DONATE);
-
-		//checkIab();
-
+		Preference gitskarios = findPreference(GITSKARIOS);
+		gitskarios.setOnPreferenceClickListener(this);
 	}
-/*
-	private void checkIab() {
-		iabHelper = new IabHelper(getActivity(), IabConstants.KEY);
-		iabHelper.startSetup(this);
-	}*/
-
 	@Override
 	public boolean onPreferenceClick(Preference preference) {
 		if (preference.getKey().equals(REAUTHORIZE)) {
@@ -76,9 +59,10 @@ public class GitskariosPreferenceFragment extends PreferenceFragment implements 
 			loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			startActivity(loginIntent);
 			getActivity().finish();
-		} /*else if (preference.getKey().equals(DONATE)) {
-
-		}*/
+		} else if (preference.getKey().equals(GITSKARIOS)) {
+			Intent intent = RepoDetailActivity.createLauncherIntent(getActivity(), "alorma", "gitskarios");
+			startActivity(intent);
+		}
 		return true;
 	}
 
@@ -101,66 +85,4 @@ public class GitskariosPreferenceFragment extends PreferenceFragment implements 
 		}
 		return true;
 	}
-
-	/*@Override
-	public void onIabSetupFinished(IabResult result) {
-		iabEnabled = result.isSuccess();
-		if (iabEnabled) {
-			try {
-				iabHelper.queryInventoryAsync(this);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		if (iabHelper != null) {
-			iabHelper.dispose();
-		}
-		iabHelper = null;
-	}
-
-	@Override
-	public void onIabPurchaseFinished(IabResult result, Purchase info) {
-		if (result.isFailure()) {
-			invalidateDonateOption();
-		} else if (info.getSku().equals(IabConstants.SKU_DONATE)) {
-			SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-			SharedPreferences.Editor editor = preferences.edit();
-			editor.putBoolean(IabConstants.SKU_DONATE, true);
-			editor.apply();
-			invalidateDonateOption();
-		}
-	}
-
-	@Override
-	public void onQueryInventoryFinished(IabResult result, Inventory inv) {
-		if (result.isFailure()) {
-			if (result.getResponse() == IabHelper.BILLING_RESPONSE_RESULT_DEVELOPER_ERROR) {
-				SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-				SharedPreferences.Editor editor = preferences.edit();
-				editor.putBoolean(IabConstants.SKU_DONATE, true);
-				editor.apply();
-			}
-			invalidateDonateOption();
-		} else {
-			boolean iabDonatePurchased = inv.hasPurchase(IabConstants.SKU_DONATE);
-			if (iabDonatePurchased) {
-				SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-				SharedPreferences.Editor editor = preferences.edit();
-				editor.putBoolean(IabConstants.SKU_DONATE, true);
-				editor.apply();
-			}
-			invalidateDonateOption();
-		}
-	}
-
-	private void invalidateDonateOption() {
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-		donate.setEnabled(!preferences.getBoolean(IabConstants.SKU_DONATE, false));
-	}
-*/
 }
