48656cca35b5ff96c84f546902ae89057bc4f7f8
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-02-27 23:29:50 +0100

    Notifications row header with mark as read icon

--- app/src/main/java/com/alorma/github/ui/adapter/NotificationsAdapter.java
+++ app/src/main/java/com/alorma/github/ui/adapter/NotificationsAdapter.java
@@ -1,14 +1,19 @@
 package com.alorma.github.ui.adapter;
 
 import android.content.Context;
+import android.support.v7.internal.widget.ViewUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alorma.github.R;
 import com.alorma.github.sdk.bean.dto.response.Notification;
+import com.alorma.github.utils.AttributesUtils;
+import com.alorma.githubicons.GithubIconDrawable;
+import com.alorma.githubicons.GithubIconify;
 
 import java.util.List;
 
@@ -41,10 +46,16 @@ public class NotificationsAdapter extends ArrayAdapter<Notification> implements 
 
 	@Override
 	public View getHeaderView(int i, View view, ViewGroup viewGroup) {
-		View v = mInflater.inflate(R.layout.commit_row_header, viewGroup, false);
+		View v = mInflater.inflate(R.layout.notification_row_header, viewGroup, false);
 		TextView tv = (TextView) v.findViewById(android.R.id.text1);
+		ImageView iv = (ImageView) v.findViewById(R.id.clearNotifications);
 
 		Notification item = getItem(i);
+
+		GithubIconDrawable iconDrawable = new GithubIconDrawable(getContext(), GithubIconify.IconValue.octicon_check);
+		iconDrawable.sizeRes(R.dimen.header_height);
+		iconDrawable.color(AttributesUtils.getIconsColor(getContext(), R.style.AppTheme_Repos));
+		iv.setImageDrawable(iconDrawable);
 
 		tv.setText(item.repository.full_name);
 
