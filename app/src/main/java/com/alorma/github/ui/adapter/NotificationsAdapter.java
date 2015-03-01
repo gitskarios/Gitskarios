be66ea4a83a3eea000df09b4fab514a41f403963
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-03-01 12:05:30 +0100

    Notifications check for clean

--- app/src/main/java/com/alorma/github/ui/adapter/NotificationsAdapter.java
+++ app/src/main/java/com/alorma/github/ui/adapter/NotificationsAdapter.java
@@ -1,13 +1,16 @@
 package com.alorma.github.ui.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.internal.widget.ViewUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alorma.github.R;
 import com.alorma.github.sdk.bean.dto.response.Notification;
@@ -25,10 +28,15 @@ import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
 public class NotificationsAdapter extends ArrayAdapter<Notification> implements StickyListHeadersAdapter {
 
 	private final LayoutInflater mInflater;
+	private final GithubIconDrawable iconDrawable;
 
 	public NotificationsAdapter(Context context, List<Notification> notifications) {
 		super(context, 0, notifications);
 		mInflater = LayoutInflater.from(context);
+
+		iconDrawable = new GithubIconDrawable(getContext(), GithubIconify.IconValue.octicon_check);
+		iconDrawable.sizeRes(R.dimen.gapLarge);
+		iconDrawable.color(AttributesUtils.getSecondaryTextColor(getContext(), R.style.AppTheme_Repos));
 	}
 
 	@Override
@@ -47,19 +55,23 @@ public class NotificationsAdapter extends ArrayAdapter<Notification> implements 
 	@Override
 	public View getHeaderView(int i, View view, ViewGroup viewGroup) {
 		View v = mInflater.inflate(R.layout.notification_row_header, viewGroup, false);
-		TextView tv = (TextView) v.findViewById(android.R.id.text1);
-		ImageView iv = (ImageView) v.findViewById(R.id.clearNotifications);
 
-		Notification item = getItem(i);
+		final Notification item = getItem(i);
 
-		GithubIconDrawable iconDrawable = new GithubIconDrawable(getContext(), GithubIconify.IconValue.octicon_check);
-		iconDrawable.sizeRes(R.dimen.header_height);
-		iconDrawable.color(AttributesUtils.getIconsColor(getContext(), R.style.AppTheme_Repos));
-		iv.setImageDrawable(iconDrawable);
-
+		TextView tv = (TextView) v.findViewById(R.id.text);
 		tv.setText(item.repository.full_name);
 
-		return tv;
+		ImageView iv = (ImageView) v.findViewById(R.id.clearNotifications);
+		iv.setImageDrawable(iconDrawable);
+		
+		iv.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(v.getContext(), item.repository.full_name, Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		return v;
 	}
 
 	@Override
