248f4d58976b9924b0b67767d465ad8de4f5f683
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-03-31 18:31:08 +0200

    Human readable quota

--- app/src/main/java/com/alorma/github/ui/cards/profile/GithubPlanCard.java
+++ app/src/main/java/com/alorma/github/ui/cards/profile/GithubPlanCard.java
@@ -56,8 +56,15 @@ public class GithubPlanCard implements View.OnClickListener {
 
 		TextView text = (TextView) view.findViewById(R.id.textPlanQuota);
 
-		text.setText(view.getContext().getString(R.string.quota_data, String.valueOf(user.plan.space)));
+		text.setText(view.getContext().getString(R.string.quota_data, humanReadableByteCount(user.plan.space, true)));
+	}
 
+	public static String humanReadableByteCount(long bytes, boolean si) {
+		int unit = si ? 1000 : 1024;
+		if (bytes < unit) return bytes + " B";
+		int exp = (int) (Math.log(bytes) / Math.log(unit));
+		String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+		return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
 	}
 
 	private void setUpCollaborators(View view) {
