6d2088359bf2d7fa36ace62239f565d0efab6b8b
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-04-02 15:22:09 +0200

    Change account

--- gistsapp/src/main/java/com/alorma/gistsapp/MainActivity.java
+++ gistsapp/src/main/java/com/alorma/gistsapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.alorma.gistsapp;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,10 +11,16 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.alorma.github.sdk.bean.dto.response.ListGists;
+import com.alorma.github.sdk.bean.dto.response.User;
 import com.alorma.github.sdk.login.AccountsHelper;
 import com.alorma.github.sdk.security.StoreCredentials;
+import com.alorma.github.sdk.services.client.BaseClient;
+import com.alorma.github.sdk.services.gists.UserGistsClient;
 import com.alorma.githubicons.GithubIconDrawable;
 import com.alorma.githubicons.GithubIconify;
 import com.mikepenz.google_material_typeface_library.GoogleMaterial;
@@ -27,28 +34,22 @@ import com.mikepenz.materialdrawer.util.DrawerImageLoader;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends ActionBarActivity {
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class MainActivity extends ActionBarActivity implements BaseClient.OnResultCallback<ListGists> {
 
     private Toolbar toolbar;
     private AccountHeader.Result header;
+    private Account selectedAccount;
+    private HashMap<String, Account> accountMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-        /*if (accounts.length > 0) {
-            Account account = accounts[0];
-            StoreCredentials credentials = new StoreCredentials(MainActivity.this);
-            credentials.clear();
-            String authToken = AccountsHelper.getUserToken(this, account);
-
-            credentials.storeToken(authToken);
-            credentials.storeUsername(account.name);
-        }*/
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -75,7 +76,24 @@ public class MainActivity extends ActionBarActivity {
 
         AccountHeader accountHeaderBuilder = new AccountHeader()
                 .withActivity(this)
-                .withHeaderBackground(R.drawable.header);
+                .withHeaderBackground(R.drawable.header)
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile iProfile, boolean b) {
+                        if (iProfile.getIdentifier() != -1) {
+                            Account account = accountMap.get(iProfile.getName());
+                            selectAccount(account);
+                            return false;
+                        } else {
+                            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                            intent.putExtra(LoginActivity.ADDING_FROM_APP, true);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                            finish();
+                            return true;
+                        }
+                    }
+                });
 
         header = accountHeaderBuilder.build();
 
@@ -113,16 +131,45 @@ public class MainActivity extends ActionBarActivity {
 
                 header.addProfiles(itemAdd);
             }
+            selectAccount(accounts[0]);
         }
     }
 
-
     private void addAccountToHeader(Account account, int i) {
+        accountMap.put(account.name, account);
         String userAvatar = AccountsHelper.getUserAvatar(this, account);
         String userMail = AccountsHelper.getUserMail(this, account);
         String userName = AccountsHelper.getUserName(this, account);
         ProfileDrawerItem profileDrawerItem = new ProfileDrawerItem().withName(account.name).withIcon(userAvatar)
                 .withEmail(userMail != null ? userMail : userName).withIdentifier(i);
         header.addProfiles(profileDrawerItem);
+    }
+
+    private void selectAccount(Account account) {
+        this.selectedAccount = account;
+        StoreCredentials credentials = new StoreCredentials(MainActivity.this);
+        credentials.clear();
+        String authToken = AccountsHelper.getUserToken(this, account);
+
+        credentials.storeToken(authToken);
+        credentials.storeUsername(account.name);
+
+        loadGists();
+    }
+
+    private void loadGists() {
+        UserGistsClient gistsClient = new UserGistsClient(this);
+        gistsClient.setOnResultCallback(this);
+        gistsClient.execute();
+    }
+
+    @Override
+    public void onResponseOk(ListGists gists, Response r) {
+        Toast.makeText(this, "Gists: " + gists.size(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onFail(RetrofitError error) {
+        Toast.makeText(this, "Error: " + error, Toast.LENGTH_SHORT).show();
     }
 }
