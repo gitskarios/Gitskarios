528e661d22f4151b8a26bf26bc0d4c077eb136bf
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-04-03 19:46:24 +0200

    Add editor. Add note empty discard

--- gistsapp/src/main/java/com/alorma/gistsapp/ui/activity/CreateGistActivity.java
+++ gistsapp/src/main/java/com/alorma/gistsapp/ui/activity/CreateGistActivity.java
@@ -1,20 +1,32 @@
 package com.alorma.gistsapp.ui.activity;
 
 import android.app.AlertDialog;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.alorma.gistsapp.R;
+import com.alorma.gistsapp.ui.adapter.GistDetailFilesAdapter;
+import com.alorma.gistsapp.ui.fragment.GistEditorFragment;
+import com.alorma.github.sdk.bean.dto.response.GistFile;
+import com.github.mrengineer13.snackbar.SnackBar;
 
 /**
  * Created by Bernat on 02/04/2015.
  */
-public class CreateGistActivity extends ActionBarActivity {
+public class CreateGistActivity extends ActionBarActivity implements GistEditorFragment.GistEditorListener {
+    private RecyclerView recyclerView;
+    private GistDetailFilesAdapter adapter;
+    private GistEditorFragment editorFragment;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,13 +43,38 @@ public class CreateGistActivity extends ActionBarActivity {
         if (sharingMode) {
             String text = getIntent().getStringExtra(Intent.EXTRA_TEXT);
             if (!TextUtils.isEmpty(text)) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Add snippet");
-                builder.setMessage(text);
-                builder.show();
+                launchEditor(text);
             }
         }
 
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(getResources().getInteger(R.integer.gist_files_count), StaggeredGridLayoutManager.VERTICAL));
+
+        adapter = new GistDetailFilesAdapter(this);
+        recyclerView.setAdapter(adapter);
+
+        findViewById(R.id.fabButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchEditor(null);
+            }
+        });
+    }
+
+    private void launchEditor(String text) {
+        editorFragment = GistEditorFragment.newInstance();
+        editorFragment.setGistEditorListener(this);
+
+        boolean fullScreen = getResources().getBoolean(R.bool.editor_fullscreen);
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        if (fullScreen) {
+            ft.replace(android.R.id.content, editorFragment);
+            ft.addToBackStack(null);
+            ft.commit();
+        } else {
+            editorFragment.show(ft, "editor");
+        }
     }
 
     @Override
@@ -47,5 +84,19 @@ public class CreateGistActivity extends ActionBarActivity {
         }
 
         return true;
+    }
+
+    @Override
+    public void onGistEditorFinish(GistFile file) {
+        if (editorFragment != null) {
+            editorFragment.setGistEditorListener(null);
+            getFragmentManager().beginTransaction().remove(editorFragment).commit();
+
+            if (TextUtils.isEmpty(file.filename) && TextUtils.isEmpty(file.content)) {
+                new SnackBar.Builder(this)
+                        .withMessageId(R.string.editor_discard)
+                        .show();
+            }
+        }
     }
 }
---
+++ gistsapp/src/main/java/com/alorma/gistsapp/ui/fragment/GistEditorFragment.java
@@ -0,0 +1,87 @@
+package com.alorma.gistsapp.ui.fragment;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Toast;
+
+import com.alorma.gistsapp.R;
+import com.alorma.github.sdk.bean.dto.response.GistFile;
+
+/**
+ * Created by Bernat on 03/04/2015.
+ */
+public class GistEditorFragment extends DialogFragment {
+
+    private GistEditorListener gistEditorListener;
+
+    public static GistEditorFragment newInstance() {
+        GistEditorFragment editorFragment = new GistEditorFragment();
+        editorFragment.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+        return editorFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.editor_fragment, null, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                endGistFile();
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Dialog dialog = getDialog();
+        // request a window without the title
+        if (dialog != null) {
+            int dialogWidth = getResources().getDimensionPixelOffset(R.dimen.editor_popup_width);
+            int dialogHeight = getResources().getDisplayMetrics().heightPixels;
+
+            dialog.getWindow().setLayout(dialogWidth, dialogHeight);
+        }
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        endGistFile();
+        super.onDismiss(dialog);
+    }
+
+    private void endGistFile() {
+        if (gistEditorListener != null) {
+            GistFile file = new GistFile();
+            gistEditorListener.onGistEditorFinish(file);
+        }
+    }
+
+    public void setGistEditorListener(GistEditorListener gistEditorListener) {
+        this.gistEditorListener = gistEditorListener;
+    }
+
+    public interface GistEditorListener {
+        void onGistEditorFinish(GistFile file);
+    }
+}
