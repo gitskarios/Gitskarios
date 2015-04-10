162b0e483973438b4771b4f54366364ffd56bd21
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-04-10 21:15:41 +0200

    Issue activity functionality

--- app/src/main/java/com/alorma/github/ui/activity/IssueDetailActivity.java
+++ app/src/main/java/com/alorma/github/ui/activity/IssueDetailActivity.java
@@ -8,24 +8,25 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.BounceInterpolator;
-import android.view.animation.LinearInterpolator;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.alorma.github.R;
 import com.alorma.github.sdk.bean.dto.response.Issue;
 import com.alorma.github.sdk.bean.dto.response.IssueState;
 import com.alorma.github.sdk.bean.dto.response.Permissions;
-import com.alorma.github.sdk.bean.dto.response.User;
 import com.alorma.github.sdk.bean.info.IssueInfo;
 import com.alorma.github.sdk.bean.issue.IssueStory;
 import com.alorma.github.sdk.services.client.BaseClient;
+import com.alorma.github.sdk.services.issues.CloseIssueClient;
 import com.alorma.github.sdk.services.issues.story.IssueStoryLoader;
 import com.alorma.github.ui.activity.base.BackActivity;
 import com.alorma.github.ui.adapter.issues.IssueDetailAdapter;
+import com.alorma.github.ui.dialog.NewIssueCommentActivity;
 import com.alorma.github.ui.fragment.detail.issue.IssueDiscussionFragment;
-import com.alorma.github.utils.AttributesUtils;
 import com.alorma.githubicons.GithubIconDrawable;
 import com.alorma.githubicons.GithubIconify;
 import com.getbase.floatingactionbutton.FloatingActionButton;
@@ -37,19 +38,19 @@ import com.nineoldandroids.animation.ValueAnimator;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class IssueDetailActivity extends BackActivity implements BaseClient.OnResultCallback<IssueStory>, View.OnClickListener /*, BaseClient.OnResultCallback<Issue>, */ {
+public class IssueDetailActivity extends BackActivity implements BaseClient.OnResultCallback<IssueStory>, View.OnClickListener {
 
     public static final String ISSUE_INFO = "ISSUE_INFO";
     public static final String PERMISSIONS = "PERMISSIONS";
     private static final int NEW_COMMENT_REQUEST = 1243;
 
     private IssueDiscussionFragment issueDiscussionFragment;
-    private IssueState issueState;
     private Permissions permissions;
     private boolean shouldRefreshOnBack;
     private IssueInfo issueInfo;
     private RecyclerView recyclerView;
     private FloatingActionButton fab;
+    private IssueStory issueStory;
 
     public static Intent createLauncherIntent(Context context, IssueInfo issueInfo, Permissions permissions) {
         Bundle bundle = new Bundle();
@@ -104,6 +105,11 @@ public class IssueDetailActivity extends BackActivity implements BaseClient.OnRe
 
     @Override
     public void onResponseOk(IssueStory issueStory, Response r) {
+        this.issueStory = issueStory;
+        applyIssue();
+    }
+
+    private void applyIssue() {
         changeColor(issueStory.issue);
 
         fab.setVisibility(issueStory.issue.locked ? View.GONE : View.VISIBLE);
@@ -124,6 +130,8 @@ public class IssueDetailActivity extends BackActivity implements BaseClient.OnRe
         setTitle("#" + issueStory.issue.number + " " + status);
         IssueDetailAdapter adapter = new IssueDetailAdapter(this, getLayoutInflater(), issueStory);
         recyclerView.setAdapter(adapter);
+
+        invalidateOptionsMenu();
     }
 
     private void changeColor(Issue issue) {
@@ -214,65 +222,38 @@ public class IssueDetailActivity extends BackActivity implements BaseClient.OnRe
 
     @Override
     public void onClick(View view) {
-
-    }
-
-
-/*
-
-
-
-    private void setData() {
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(issue.title);
-        }
-
-        addDiscussionFragment();
-    }
-
-    private void addDiscussionFragment() {
-        try {
-            if (!isFinishing()) {
-                issueDiscussionFragment = IssueDiscussionFragment.newInstance(issueInfo);
-
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.replace(R.id.discussionFeed, issueDiscussionFragment);
-                ft.commit();
+        if (view.getId() == fab.getId()) {
+            if (!issueStory.issue.locked) {
+                onAddComment();
             }
-        } catch (IllegalStateException e) {
-            Crashlytics.logException(e);
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.issue_detail, menu);
+        if (this.issueStory != null) {
+            getMenuInflater().inflate(R.menu.issue_detail, menu);
+        }
         return true;
     }
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        menu.clear();
-
-        if (permissions != null && permissions.push && issueState == IssueState.open) {
-            MenuItem menuItem = menu.add(0, R.id.action_close_issue, 1, getString(R.string.closeIssue));
-            menuItem.setIcon(new GithubIconDrawable(this, GithubIconify.IconValue.octicon_x).actionBarSize().colorRes(R.color.white));
-            menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        if (this.issueStory != null) {
+            menu.clear();
+            if (permissions != null && permissions.push && issueStory.issue.state == IssueState.open) {
+                MenuItem menuItem = menu.add(0, R.id.action_close_issue, 1, getString(R.string.closeIssue));
+                menuItem.setIcon(new GithubIconDrawable(this, GithubIconify.IconValue.octicon_x).actionBarSize().colorRes(R.color.white));
+                menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+            }
         }
-
-        MenuItem menuItem;
-        GithubIconify.IconValue icon;
-        if (fabLayout.isFold()) {
-            menuItem = menu.add(0, R.id.action_fold_issue, 0, R.string.unfold_issue);
-            icon = GithubIconify.IconValue.octicon_unfold;
-        } else {
-            menuItem = menu.add(0, R.id.action_fold_issue, 0, R.string.fold_issue);
-            icon = GithubIconify.IconValue.octicon_fold;
-        }
-        menuItem.setIcon(new GithubIconDrawable(this, icon).actionBarSize().colorRes(R.color.white));
-        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
 
         return true;
+    }
+
+    public void onAddComment() {
+        Intent intent = NewIssueCommentActivity.launchIntent(IssueDetailActivity.this, issueInfo);
+        startActivityForResult(intent, NEW_COMMENT_REQUEST);
     }
 
     @Override
@@ -288,24 +269,11 @@ public class IssueDetailActivity extends BackActivity implements BaseClient.OnRe
                 closeIssueDialog();
                 break;
             case R.id.action_fold_issue:
-                if (fabLayout.isFold()) {
-                    issueBody.setMaxLines(Integer.MAX_VALUE);
-                    issueBody.setEllipsize(null);
-                } else {
-                    issueBody.setMaxLines(3);
-                    issueBody.setEllipsize(TextUtils.TruncateAt.END);
-                }
-                fabLayout.setFold(!fabLayout.isFold());
                 invalidateOptionsMenu();
                 break;
         }
 
         return true;
-    }
-
-    public void onAddComment() {
-        Intent intent = NewIssueCommentActivity.launchIntent(IssueDetailActivity.this, issueInfo);
-        startActivityForResult(intent, NEW_COMMENT_REQUEST);
     }
 
     private void closeIssueDialog() {
@@ -331,35 +299,22 @@ public class IssueDetailActivity extends BackActivity implements BaseClient.OnRe
 
     private void closeIssue() {
         CloseIssueClient closeIssueClient = new CloseIssueClient(this, issueInfo.repo.owner, issueInfo.repo.name, issueInfo.num);
-        closeIssueClient.setOnResultCallback(this);
+        closeIssueClient.setOnResultCallback(new BaseClient.OnResultCallback<Issue>() {
+            @Override
+            public void onResponseOk(Issue issue, Response r) {
+                if (issue != null) {
+                    getContent();
+                    IssueDetailActivity.this.issueStory.issue = issue;
+                    shouldRefreshOnBack = true;
+                }
+            }
+
+            @Override
+            public void onFail(RetrofitError error) {
+
+            }
+        });
         closeIssueClient.execute();
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == fabLayout.getFabId()) {
-            onAddComment();
-        }
-    }
-
-    @Override
-    public void onResponseOk(Issue issue, Response r) {
-        if (issue != null) {
-            issueBody.setText(issue.body);
-            IssueDetailActivity.this.issue = issue;
-            IssueDetailActivity.this.issueState = issue.state;
-            invalidateOptionsMenu();
-            setData();
-
-            //Spanned issueNumber = Html.fromHtml(getString(R.string.issue_detail_title, issue.number));
-
-            shouldRefreshOnBack = true;
-        }
-    }
-
-    @Override
-    public void onFail(RetrofitError error) {
-        ErrorHandler.onRetrofitError(IssueDetailActivity.this, "Closing issue: ", error);
     }
 
     @Override
@@ -373,8 +328,8 @@ public class IssueDetailActivity extends BackActivity implements BaseClient.OnRe
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             if (requestCode == NEW_COMMENT_REQUEST) {
-                addDiscussionFragment();
+                getContent();
             }
         }
-    }*/
+    }
 }
