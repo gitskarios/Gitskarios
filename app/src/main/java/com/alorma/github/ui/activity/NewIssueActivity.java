8172af4fbd40553f278957475e8b8ce3699c1966
Alexander Ivanov <vanogrid@gmail.com>
2015-07-09 00:16:18 +0300

    Fix #155

--- app/src/main/java/com/alorma/github/ui/activity/NewIssueActivity.java
+++ app/src/main/java/com/alorma/github/ui/activity/NewIssueActivity.java
@@ -217,6 +217,7 @@ public class NewIssueActivity extends BackActivity implements BaseClient.OnResul
             issueInfo.num = issue.number;
             Intent launcherIntent = IssueDetailActivity.createLauncherIntent(this, issueInfo);
             startActivity(launcherIntent);
+            setResult(RESULT_OK);
             finish();
         }
     }
