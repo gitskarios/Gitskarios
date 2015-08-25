9bf3cb4af2cdde311f922ca2dc905c42c07d0969
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-08-25 21:10:30 +0200

    Update SDK

--- app/src/main/java/com/alorma/github/emoji/EmojisProvider.java
+++ app/src/main/java/com/alorma/github/emoji/EmojisProvider.java
@@ -3,6 +3,7 @@ package com.alorma.github.emoji;
 import android.content.Context;
 
 import com.alorma.github.basesdk.client.BaseClient;
+import com.alorma.github.sdk.services.emojis.EmojisClient;
 
 import java.util.ArrayList;
 import java.util.HashMap;
