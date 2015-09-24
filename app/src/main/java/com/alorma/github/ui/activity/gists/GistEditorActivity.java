fef3d382992aa2fc08e70e4a1230ba0b744aa5cd
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-09-24 21:07:28 +0200

    Refactor gist file creator

--- app/src/main/java/com/alorma/github/ui/activity/gists/GistEditorActivity.java
+++ app/src/main/java/com/alorma/github/ui/activity/gists/GistEditorActivity.java
@@ -1,6 +1,25 @@
 package com.alorma.github.ui.activity.gists;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
