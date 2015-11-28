1db17d1f314a5e10e8c6e5408e5fc0832349e971
Bernat Borras <bernatbor15@gmail.com>
2015-11-28 19:56:38 +0100

    Add share Answers

--- app/src/main/java/com/alorma/github/ui/actions/ShareAction.java
+++ app/src/main/java/com/alorma/github/ui/actions/ShareAction.java
@@ -3,6 +3,9 @@ package com.alorma.github.ui.actions;
 import android.content.Context;
 import android.content.Intent;
 import com.alorma.github.R;
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.answers.ShareEvent;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Bernat on 12/10/2015.
@@ -26,6 +29,10 @@ public class ShareAction extends Action<Void> {
     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
     intent.putExtra(Intent.EXTRA_SUBJECT, title);
     intent.putExtra(Intent.EXTRA_TEXT, url);
+
+    if (Fabric.isInitialized()) {
+      Answers.getInstance().logShare(new ShareEvent());
+    }
 
     Intent chooser = Intent.createChooser(intent, context.getResources().getString(R.string.share_intent_title));
     context.startActivity(chooser);
