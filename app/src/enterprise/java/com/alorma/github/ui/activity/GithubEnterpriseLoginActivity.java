c175edebdad1ffbf034657d72d298768e77da392
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-12-10 21:31:39 +0100

    add new login enterprise screen

--- app/src/enterprise/java/com/alorma/github/ui/activity/GithubEnterpriseLoginActivity.java
+++ app/src/enterprise/java/com/alorma/github/ui/activity/GithubEnterpriseLoginActivity.java
@@ -5,10 +5,12 @@ import android.accounts.AccountAuthenticatorActivity;
 import android.accounts.AccountManager;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -35,17 +37,22 @@ public class GithubEnterpriseLoginActivity extends AccountAuthenticatorActivity 
 
     @Bind(R.id.enterpriseUrl)
     EditText enterpriseUrl;
-    @Bind(R.id.enterpriseToken)
+    @Bind(R.id.accessToken)
     EditText enterpriseToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_github_enterprise_login);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        }
+
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.enterpriseGenerateToken)
+    @OnClick(R.id.generateToken)
     public void generateToken() {
         if (enterpriseUrl.length() > 0) {
             String url = enterpriseUrl.getText().toString() + "/settings/tokens";
@@ -54,7 +61,7 @@ public class GithubEnterpriseLoginActivity extends AccountAuthenticatorActivity 
         }
     }
 
-    @OnClick(R.id.enterpriseLogin)
+    @OnClick(R.id.loginGithub)
     public void onLogin() {
         if (enterpriseUrl.length() > 0 && enterpriseToken.length() > 0) {
             final String url = enterpriseUrl.getText().toString();
