cfa63cd5497fbe219f68ed4ea6572f2e07910c9c
Bernat Borrás Paronella <bernatbor15@gmail.com>
2016-01-04 13:34:35 +0100

    add check ad into enterprise

---
+++ app/src/enterprise/java/com/alorma/github/ui/activity/CheckShowAd.java
@@ -0,0 +1,12 @@
+package com.alorma.github.ui.activity;
+
+import android.content.Context;
+
+/**
+ * Created by bernat.borras on 4/1/16.
+ */
+public class CheckShowAd {
+    public static void showEnterpriseAd(Context context) {
+
+    }
+}
