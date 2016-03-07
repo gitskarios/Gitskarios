f3e3c410c697bdc9345b86f9e8d3ea7edc1b5939
Bernat Borrás Paronella <bernatbor15@gmail.com>
2016-03-07 16:15:37 +0100

    fix gcm not loading

---
+++ app/src/main/java/com/alorma/github/gcm/GitskariosRegistrationService.java
@@ -0,0 +1,33 @@
+package com.alorma.github.gcm;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+import com.alorma.github.GitskariosSettings;
+import com.alorma.github.R;
+import com.google.android.gms.gcm.GcmPubSub;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+import java.io.IOException;
+
+public class GitskariosRegistrationService extends IntentService {
+  /**
+   * Creates an IntentService.  Invoked by your subclass's constructor.
+   *
+   */
+  public GitskariosRegistrationService() {
+    super("GitskariosRegistrationService");
+  }
+
+  @Override
+  protected void onHandleIntent(Intent intent) {
+    try {
+      InstanceID instanceID = InstanceID.getInstance(this);
+      String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+          GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+      new GitskariosSettings(this).saveGCMToken(token);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
