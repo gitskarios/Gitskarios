c11ac1715d359fb04c8a37d2cb983c90b9c02147
Bernat Borrás Paronella <bernatbor15@gmail.com>
2016-05-15 18:18:43 +0200

    Improve emojis

--- app/src/main/java/com/alorma/github/ui/view/issue/ReactionsView.java
+++ app/src/main/java/com/alorma/github/ui/view/issue/ReactionsView.java
@@ -2,20 +2,17 @@ package com.alorma.github.ui.view.issue;
 
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.support.v4.content.res.ResourcesCompat;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.alorma.github.R;
 import com.alorma.github.sdk.bean.dto.response.GithubCommentReactions;
-import com.alorma.github.sdk.bean.dto.response.GithubReaction;
+import com.alorma.github.sdk.services.issues.reactions.GithubReaction;
 
-import java.util.ArrayList;
 import java.util.List;
+
 public class ReactionsView extends LinearLayout {
 
 
@@ -48,7 +45,7 @@ public class ReactionsView extends LinearLayout {
 
         TextView reaction1 = (TextView) findViewById(R.id.reaction1);
         TextView reaction2 = (TextView) findViewById(R.id.reaction2);
-        TextView reaction3  = (TextView) findViewById(R.id.reaction3);
+        TextView reaction3 = (TextView) findViewById(R.id.reaction3);
         TextView reaction4 = (TextView) findViewById(R.id.reaction4);
         TextView reaction5 = (TextView) findViewById(R.id.reaction5);
         TextView reaction6 = (TextView) findViewById(R.id.reaction6);
@@ -59,63 +56,13 @@ public class ReactionsView extends LinearLayout {
 
     public void setReactions(GithubCommentReactions reactions) {
         if (reactions != null) {
-            List<GithubReaction> reactionList = new ArrayList<>();
-
-            if (reactions.getPlusOne() > 0) {
-                reactionList.add(new GithubReaction("plusOne", reactions.getPlusOne()));
-            }
-            if (reactions.getMinusOne() > 0) {
-                reactionList.add(new GithubReaction("minusOne", reactions.getMinusOne()));
-            }
-            if (reactions.getConfused() > 0) {
-                reactionList.add(new GithubReaction("confused", reactions.getConfused()));
-            }
-            if (reactions.getLaugh() > 0) {
-                reactionList.add(new GithubReaction("laugh", reactions.getLaugh()));
-            }
-            if (reactions.getHooray() > 0) {
-                reactionList.add(new GithubReaction("hooray", reactions.getHooray()));
-            }
-            if (reactions.getHeart() > 0) {
-                reactionList.add(new GithubReaction("heart", reactions.getHeart()));
-            }
-
+            List<GithubReaction> reactionList = reactions.getReactions();
             int min = Math.min(this.reactions.length, reactionList.size());
             for (int i = 0; i < min; i++) {
                 TextView reaction = this.reactions[i];
-
-                GithubReaction githubReaction = reactionList.get(i);
-
-                int icon = getIconForReaction(githubReaction);
-
-                String text = String.valueOf(githubReaction.getValue());
-                if (icon != 0) {
-                    text = getEmijoByUnicode(icon) + " " + text;
-                }
-
-                reaction.setText(text);
+                reaction.setText(reactionList.get(i).toString());
             }
-        }
-    }
 
-    public String getEmijoByUnicode(int unicode){
-        return new String(Character.toChars(unicode));
-    }
-
-    private int getIconForReaction(GithubReaction githubReaction) {
-        if (githubReaction.getContent().equals("plusOne")) {
-            return 0x1F44D;
-        } else if (githubReaction.getContent().equals("minusOne")) {
-            return 0x1F44E;
-        } else if (githubReaction.getContent().equals("confused")) {
-            return 0x1F615;
-        } else if (githubReaction.getContent().equals("laugh")) {
-            return 0x1F604;
-        } else if (githubReaction.getContent().equals("hooray")) {
-            return 0x1F389;
-        } else if (githubReaction.getContent().equals("heart")) {
-            return 0x2764;
         }
-        return 0;
     }
 }
--- app/src/main/java/com/alorma/github/utils/KeyboardUtils.java
+++ app/src/main/java/com/alorma/github/utils/KeyboardUtils.java
@@ -16,26 +16,34 @@ public class KeyboardUtils {
     /**
      * Lower a keyboard owned by an activity window.
      *
-     * @param activity  Current visible instance of activity window
+     * @param activity Current visible instance of activity window
      */
     public static void lowerKeyboard(@NonNull Activity activity) {
-        if (activity.getWindow() != null) {
-            View decorView = activity.getWindow().getDecorView();
-            lowerKeyboard(activity, decorView);
+        try {
+            if (activity.getWindow() != null) {
+                View decorView = activity.getWindow().getDecorView();
+                lowerKeyboard(activity, decorView);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     /**
      * Lower an IEM keyboard that has been raised with a view such as dialog.
      *
-     * @param applicationContext   Context of application
-     * @param focusView View that owns keyboard.
+     * @param applicationContext Context of application
+     * @param focusView          View that owns keyboard.
      */
     public static void lowerKeyboard(@NonNull Context applicationContext, View focusView) {
-        if (focusView != null && focusView.getWindowToken() != null) {
-            InputMethodManager imm = (InputMethodManager)
-                    applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(focusView.getWindowToken(), 0);
+        try {
+            if (focusView != null && focusView.getWindowToken() != null) {
+                InputMethodManager imm = (InputMethodManager)
+                        applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(focusView.getWindowToken(), 0);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
