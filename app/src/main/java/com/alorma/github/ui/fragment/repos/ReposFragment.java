04dd1e9aed9093d892e24bff20ab4ecac9fe1768
Bernat Borrás Paronella <bernatbor15@gmail.com>
2016-06-03 19:41:43 +0200

    Fix 436

--- app/src/main/java/com/alorma/github/ui/fragment/repos/ReposFragment.java
+++ app/src/main/java/com/alorma/github/ui/fragment/repos/ReposFragment.java
@@ -2,6 +2,7 @@ package com.alorma.github.ui.fragment.repos;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,8 +10,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.widget.Toast;
 import com.alorma.github.GitskariosApplication;
 import com.alorma.github.R;
 import com.alorma.github.injector.component.ApiComponent;
@@ -23,90 +22,87 @@ import com.alorma.github.ui.adapter.base.RecyclerArrayAdapter;
 import com.alorma.github.ui.listeners.TitleProvider;
 import com.alorma.github.utils.AttributesUtils;
 import com.mikepenz.iconics.typeface.IIcon;
-
 import java.util.List;
 
 public abstract class ReposFragment extends Fragment
-        implements TitleProvider, Presenter.Callback<List<Repo>>,
-        RecyclerArrayAdapter.RecyclerAdapterContentListener {
+    implements TitleProvider, Presenter.Callback<List<Repo>>, RecyclerArrayAdapter.RecyclerAdapterContentListener {
 
-    private ReposAdapter adapter;
-    private SwipeRefreshLayout refreshLayout;
+  private ReposAdapter adapter;
+  private SwipeRefreshLayout refreshLayout;
+  private RecyclerView recyclerView;
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        GitskariosApplication application = (GitskariosApplication) getActivity().getApplication();
-        ApplicationComponent component = application.getComponent();
+    GitskariosApplication application = (GitskariosApplication) getActivity().getApplication();
+    ApplicationComponent component = application.getComponent();
 
-        ApiComponent apiComponent = DaggerApiComponent.builder()
-                .applicationComponent(component)
-                .apiModule(new ApiModule())
-                .build();
+    ApiComponent apiComponent = DaggerApiComponent.builder().applicationComponent(component).apiModule(new ApiModule()).build();
 
-        initInjectors(apiComponent);
+    initInjectors(apiComponent);
+  }
+
+  protected abstract void initInjectors(ApiComponent apiComponent);
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.recyclerview, null, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    refreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe);
+    refreshLayout.setOnRefreshListener(this::onRefresh);
+    refreshLayout.setColorSchemeColors(AttributesUtils.getAccentColor(getActivity()));
+
+    recyclerView = (RecyclerView) view.findViewById(R.id.recycler);
+    adapter = new ReposAdapter(LayoutInflater.from(getActivity()));
+    adapter.setRecyclerAdapterContentListener(this);
+    recyclerView.setAdapter(adapter);
+    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    showLoading();
+    onRefresh();
+  }
+
+  protected abstract void onRefresh();
+
+  @Override
+  public abstract int getTitle();
+
+  @Override
+  public abstract IIcon getTitleIcon();
+
+  @Override
+  public void showLoading() {
+    refreshLayout.post(() -> refreshLayout.setRefreshing(true));
+  }
+
+  @Override
+  public void onResponse(List<Repo> repos, boolean firstTime) {
+    if (firstTime) {
+      adapter.clear();
     }
+    adapter.addAll(repos);
+  }
 
-    protected abstract void initInjectors(ApiComponent apiComponent);
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.recyclerview, null, false);
+  @Override
+  public void onResponseEmpty() {
+    if (isResumed()) {
+      Snackbar.make(recyclerView, R.string.empty_repos, Snackbar.LENGTH_SHORT).show();
     }
+  }
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        refreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe);
-        refreshLayout.setOnRefreshListener(this::onRefresh);
-        refreshLayout.setColorSchemeColors(AttributesUtils.getAccentColor(getActivity()));
-
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler);
-        adapter = new ReposAdapter(LayoutInflater.from(getActivity()));
-        adapter.setRecyclerAdapterContentListener(this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        showLoading();
-        onRefresh();
-    }
-
-    protected abstract void onRefresh();
-
-    @Override
-    public abstract int getTitle();
-
-    @Override
-    public abstract IIcon getTitleIcon();
-
-    @Override
-    public void showLoading() {
-        refreshLayout.post(() -> refreshLayout.setRefreshing(true));
-    }
-
-    @Override
-    public void onResponse(List<Repo> repos, boolean firstTime) {
-        if (firstTime) {
-            adapter.clear();
-        }
-        adapter.addAll(repos);
-    }
-
-    @Override
-    public void onResponseEmpty() {
-        Toast.makeText(getActivity(), R.string.empty_repos, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void hideLoading() {
-        refreshLayout.post(() -> refreshLayout.setRefreshing(false));
-    }
+  @Override
+  public void hideLoading() {
+    refreshLayout.post(() -> refreshLayout.setRefreshing(false));
+  }
 }
