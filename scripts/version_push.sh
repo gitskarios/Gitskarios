0412d2994a8898b5c7e6fe298b9b5b51ce4e32a0
Bernat Borrás <bernatbor15@gmail.com>
2016-11-11 13:00:51 +0100

    Read version from version.properties

---
+++ scripts/version_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git config --global user.name $GIT_NAME
+git config --global user.email $GIT_EMAIL
+
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+git config --global push.default simple
+
+rev=$(git rev-parse --short HEAD)
+
+git add .
+git commit -am "From Travis Generated credentials for $TRAVIS_TAG - $rev"
+git push --set-upstream origin HEAD:master
